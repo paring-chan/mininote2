2f3590167151c5d57afa57409e4160f7baf999ab
pikokr <parangee9706@gmail.com>
2020-09-30 22:17:55 +0900

    돈랭킹 jsdoc 수정

--- src/commands/economy/moneyrank.js
+++ src/commands/economy/moneyrank.js
@@ -15,9 +15,8 @@ module.exports = class Money extends Command {
 
     /**
      * @param {Message} msg
-     * @param {{member: User}}
      */
-    async exec(msg, {member}) {
+    async exec(msg) {
         const users = (await this.client.shard.broadcastEval('this.users.cache.map(r => ({id: r.id, tag: r.tag}))')).reduce((acc,cur) => [...acc,...cur])
         await pagination(msg, async () => {
             return (await db('users').orderBy('balance', 'desc')).filter(r => users.map(r => r.id).includes(r.id)).map((r, i) => `${i+1}위: ${users.find(j => j.id === r.id).tag}`)
