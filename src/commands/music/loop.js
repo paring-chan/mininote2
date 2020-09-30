dd45c26b4e185faec3047afbfb596940340d0c25
pikokr <parangee9706@gmail.com>
2020-09-30 17:43:41 +0900

    반복 기능 추가

--- src/commands/music/loop.js
+++ src/commands/music/loop.js
@@ -18,5 +18,20 @@ module.exports = class Play extends Command {
     async exec(msg) {
         const player = this.client.music.players.get(msg.guild.id)
         if (!player) return msg.util.reply('재생 안하고있는데요')
+        let m = ''
+        if (player.trackRepeat) {
+            player.setTrackRepeat(false)
+            player.setQueueRepeat(true)
+            m = '반복이 `대기열 반복`으로 설정되었습니다.'
+        } else if (player.queueRepeat) {
+            player.setTrackRepeat(false)
+            player.setQueueRepeat(false)
+            m = '반복이 `비활성화` 되었습니다.'
+        } else {
+            player.setTrackRepeat(true)
+            player.setQueueRepeat(false)
+            m = '반복이 `곡 반복` 으로 설정되었습니다.'
+        }
+        await msg.util.send(m)
     }
 }
