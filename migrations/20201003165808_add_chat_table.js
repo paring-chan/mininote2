be81376484fc3996654e89195696bc060cbdc244
pikokr <parangee9706@gmail.com>
2020-10-03 17:09:07 +0900

    마이그레이션 추가(수정 완료)

--- migrations/20201003165808_add_chat_table.js
+++ migrations/20201003165808_add_chat_table.js
@@ -1,8 +1,20 @@
+const Knex = require("knex");
 
+/**
+ * @param {Knex} knex
+ */
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('chat', t => {
+        t.string('id').notNullable()
+        t.string('question').notNullable()
+        t.string('response').notNullable()
+    })
 };
 
+
+/**
+ * @param {Knex} knex
+ */
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('chat')
 };
