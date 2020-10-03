1aa530f1b03a5e95918587371f2092e3b5fceecc
pikokr <parangee9706@gmail.com>
2020-10-03 15:15:18 +0900

    갈고리 수집 where이 없었다

--- src/listeners/message.js
+++ src/listeners/message.js
@@ -17,7 +17,7 @@ module.exports = class CommandError extends Listener {
         if (msg.content === '?') {
             const user = (await db('users').where({id: msg.author.id}))[0]
             if (!user) return
-            await db('users').increment('galgories', 1)
+            await db('users').increment('galgories', 1).where('id', msg.author.id)
             await msg.channel.send(`갈고리 줍줍(${user.galgories+1}개)`)
         }
     }
